d83fdd7b9bd71cc2f1e2a37a4dfe7f8e7205e53d
Manuel Gomez <jmgc@virket.com>
2024-07-19 08:41:15 -0600

    Preparación de archivos y directorios

---
+++ src/presentation/screens/auth/RegisterScreen.tsx
@@ -0,0 +1,12 @@
+
+import { Text, View } from 'react-native'
+
+export const ResgisterScreen = () => {
+    return (
+        <View>
+            <Text>
+                ResgisterScreen
+            </Text>
+        </View>
+    )
+}
