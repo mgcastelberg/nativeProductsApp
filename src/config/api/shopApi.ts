6181df839ae0a5e7ae9c63d5a80e1a2930524aa7
Manuel Gomez <jmgc@virket.com>
2024-07-27 09:18:38 -0600

    Axios - Preparar cliente

---
+++ src/config/api/shopApi.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { STAGE, API_URL as PROD_URL, API_URL_IOS, API_URL_ANDROID } from '@env';
+import { Platform } from "react-native";
+
+export const API_URL = (STAGE === 'prod') 
+? PROD_URL 
+: Platform.OS === 'ios' 
+    ? API_URL_IOS : API_URL_ANDROID;
+
+const shopApi = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+})
+
+// Interceptors
+
+// Se expota asi para asegurarse de que se llame despues de ver config todos los interceptores
+export {
+    shopApi
+}
