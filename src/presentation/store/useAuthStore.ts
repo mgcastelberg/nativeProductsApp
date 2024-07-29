d18cfbcd40799ae55dc000bb6aa0c9d0730ef72e
Manuel Gomez <jmgc@virket.com>
2024-07-29 08:55:16 -0600

    Auth Store - Fuente central de información

---
+++ src/presentation/store/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { User } from "../../domain/entities/user";
+import { AuthStatus } from "../../infraestructure/interfaces/auth.status";
+import { authLogin } from "../../actions/auth/auth";
+
+export interface AuthState {
+    status: AuthStatus;
+    token?: string;
+    user?: User
+    
+    login: (email:string, password:string) => Promise<boolean>;
+}
+
+
+export const useAuthStore = create<AuthState>()( (set, get) => ({
+    status: 'checking',
+    token: undefined,
+    user: undefined,
+    login: async (email, password) => { 
+        
+        const resp = await authLogin(email, password);
+        if (!resp) {         
+            set({ status: 'unauthenticated', user: undefined, token: undefined });
+            return false;
+        }
+
+        // toDo Save token in local storage
+
+        set({ status: 'authenticated', user: resp.user, token: resp.token });
+
+        return true;
+    }
+}))
